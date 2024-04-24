4e7991dbad4d25cecc561837c0071149427c5282
smayank1214 <rawatmayank716@gmail.com>
2024-04-24 14:31:46 +0530

    Setup done and added assets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter} from "react-router-dom";
+import AnimatedRoutes from "./components/AnimatedRoute/AnimatedRoutes";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter className="App">
+      <ToastContainer />
+       <AnimatedRoutes/>
+    </BrowserRouter>
   );
 }
 
