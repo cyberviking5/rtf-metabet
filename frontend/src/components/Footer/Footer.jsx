ce5c53f31d90794298b2a88772ccc129f329fa64
smayank1214 <rawatmayank716@gmail.com>
2024-04-25 00:40:00 +0530

    Changes in footer made

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -18,9 +18,12 @@ const Footer = () => {
             <Link to='/fb'>FACEBOOK</Link><br />
             <Link to='/ig'>INSTAGRAM</Link><br />
             <Link to='/ln'>LINKEDIN</Link><br />
-            <a href='https://github.com/cyberviking5/fantasy'>GITHUB</a>
+            <a href='https://github.com/cyberviking5/fantasy'>GITHUB</a>          
           </div>
-          <div className='ft-right'><span>MADE BY METABOYS</span><br /><span>POWERED BY RTFIGHT</span><br /><span>METABOYS PVT LIMITED</span></div>
+          {cur =='/' ?  <div><a href='#third'>REVIEWS</a><br /><Link to='/team'>OUR TEAM</Link><br /><Link to='/games'>GAMES</Link><br /><a href="#tech"></a><a href="#first">ABOUT US</a></div>
+            :
+            <div><Link to='/'>HOME</Link><br /><Link to='/team'>OUR TEAM</Link><br /><Link to='/games'>GAMES</Link><br /><Link to='/'>ABOUT US</Link></div>
+            } 
           
         </div>
         <div className='ft-right'>
