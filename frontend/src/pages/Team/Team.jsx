5456537e209c4d8cedac30fa81a713296b681637
Ankit Yadav <mahesh9426346889@gmail.com>
2024-04-24 21:45:05 +0530

    Links updated

--- frontend/src/pages/Team/Team.jsx
+++ frontend/src/pages/Team/Team.jsx
@@ -61,7 +61,7 @@ const Team = () => {
             </div>
           </div>
           <div className='team-man'>
-            <div className='team-img' id='mr'><img src={mayank} alt="" /></div>
+            <div className='team-img' id='ay'><img src={mayank} alt="" /></div>
             <div className='tb'>
             <div className='social-img'><a href="https://www.instagram.com/smayank_12/"><FaInstagram size={40}/></a><a href="https://www.linkedin.com/in/mayank-rawat-8aa02a257"><FaLinkedin size={40}/></a><a href="https://github.com/smayank1214"><FaGithub size={40}/></a></div>
               <h1>ANKIT YADAV</h1>
@@ -70,16 +70,16 @@ const Team = () => {
             </div>
           </div>
           <div className='team-man'>
-            <div className='team-img' id='mr'><img src={mayank} alt="" /></div>
+            <div className='team-img' id='sm'><img src={mayank} alt="" /></div>
             <div className='tb'>
-            <div className='social-img'><a href="https://www.instagram.com/smayank_12/"><FaInstagram size={40}/></a><a href="https://www.linkedin.com/in/mayank-rawat-8aa02a257"><FaLinkedin size={40}/></a><a href="https://github.com/smayank1214"><FaGithub size={40}/></a></div>
+            <div className='social-img'><a href="https://www.instagram.com/sukrit_malpani?igsh=MWhxeDkzcXh6ZHRxdw=="><FaInstagram size={40}/></a><a href="https://www.linkedin.com/in/sukrit-malpani-093612255?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app "><FaLinkedin size={40}/></a><a href="https://github.com/sukritmalpani/sukritmalpani"><FaGithub size={40}/></a></div>
               <h1>SUKRIT MALPANI</h1>
               <span>FRONTEND DEVELOPER</span>
               <p>We as a team are very responsible and clear with our vision. Our mission, purpose, goals and values involved the creation of our vision. </p>
             </div>
           </div>
           <div className='team-man'>
-            <div className='team-img' id='mr'><img src={mayank} alt="" /></div>
+            <div className='team-img' id='ns'><img src={mayank} alt="" /></div>
             <div className='tb'>
             <div className='social-img'><a href="https://www.instagram.com/smayank_12/"><FaInstagram size={40}/></a><a href="https://www.linkedin.com/in/mayank-rawat-8aa02a257"><FaLinkedin size={40}/></a><a href="https://github.com/smayank1214"><FaGithub size={40}/></a></div>
               <h1>NEELESH SAXENA</h1>
